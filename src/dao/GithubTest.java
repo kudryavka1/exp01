241737c435749adc35bb8f841a1c61fb117bc160
zh <454967945@qq.com>
2020-06-22 23:02:37 +0800

    完成Servlet注册页面

--- src/dao/GithubTest.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-public class GithubTest {
-    public static void main(String[] args) {
-        System.out.println("测试");
-        System.out.println("第二次测试");
-        System.out.println("第三次测试");
-    }
-}
