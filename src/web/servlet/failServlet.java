1dfe6717ed65d0db3651b6b7165b8c0615ab31d4
zh <454967945@qq.com>
2020-06-24 22:37:30 +0800

    登陆失败页面添加跳转按钮

--- src/web/servlet/failServlet.java
+++ src/web/servlet/failServlet.java
@@ -11,7 +11,8 @@ import java.io.IOException;
 public class failServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=utf-8");
-        response.getWriter().write("登陆失败，用户名或者密码错误");
+        response.getWriter().write("登陆失败，用户名或者密码错误" +
+                "<br>"+"<a href=\"/exp01/exp01/login.html\">点击跳转到登陆页面</a>");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
